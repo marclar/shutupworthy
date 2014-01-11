52eaf681d82359a9abd530358f1c674a9321ec02
marclar <mk@mk4p.com>
2014-01-11 16:22:25 -0500

    Updating for reddit

--- scripts/contentscript.js
+++ scripts/contentscript.js
@@ -1,16 +1,42 @@
 'use strict';
 
-
 //CSS to hide undesirable elements
 var camo = '{display: none !important; position: absolute !important; top: -100000px !important; left: -100000px !important; }';
 
+function appendStyle(css){
+    var style = document.createElement("style");
+    style.type = 'text/css';
+    style.appendChild(document.createTextNode(css));
+    document.head.appendChild(style);
+};
 
 //Manifest of domains and the actions to take on each
 var domains = {
 
+    //Amazon
+    "amazon.com": {
+        "hide": ["#cart-upsell"]
+    },
+
     //Facebook
     "facebook.com": {
-        "hide": ["#pagelet_ego_pane"]
+        "hide": ["#pagelet_ego_pane", ".ego_column"]
+    },
+
+    //Forbes
+    "forbes.com": {
+        "hide": [".forbes_flyer"]
+    },
+
+    //Google properties
+    "google.com": {
+        "run": function(){
+
+            //Skip the stupid GA landing page
+            if(document.location.pathname === '/analytics/'){
+                document.location = 'https://www.google.com/analytics/web/?hl=en';
+            }
+        }
     },
 
     //Intercom
@@ -73,9 +99,50 @@ var domains = {
         }
     },
 
+    //Reddit
+    "reddit.com": {
+        "run": function(){
+            var css = [];
+            css.push('.res-nightmode .RES-keyNav-activeElement, .res-nightmode .RES-keyNav-activeElement .usertext-body, .res-nightmode .RES-keyNav-activeElement .usertext-body .md, .res-nightmode .RES-keyNav-activeElement .usertext-body .md p, .res-nightmode .commentarea .RES-keyNav-activeElement .noncollapsed, .res-nightmode .RES-keyNav-activeElement .noncollapsed .md, .res-nightmode .RES-keyNav-activeElement .noncollapsed .md p {');
+            css.push('');
+            css.push('  background-color: #000 !important;');
+            css.push('');
+            css.push('}');
+            css.push('');
+            css.push('');
+            appendStyle(css.join('\n'));
+        }
+    },
+
+    //Stackexchange
+    "security.stackexchange.com": {"hide": ["#overlay-header"]},
+    "serverfault.com": {"hide": ["#overlay-header"]},
+    "stackexchange.com": {"hide": ["#overlay-header"]},
+    "stackoverflow.com": {"hide": ["#overlay-header"]},
+
+    //Tumblr
+    "tumblr.com": {
+        "hide": ["#tumblr_teaser_follow"]
+    },
+
     //Upworthy
     "upworthy.com": {
         "hide": ["#slider", ".modal", ".modal-backdrop"]
+    },
+
+    //Washington Post
+    "washingtonpost.com": {
+        "hide": ["#single-next-article"]
+    },
+
+    //Wired
+    "wired.com": {
+        "hide": ["#post_nav"]
+    },
+
+    //Workflowy
+    "workflowy.com": {
+        "hide": ["#proPitchBottomRight", "#bottomLinks"]
     }
 
 };
